9b9636e87e1ea78b0bd06f440c681d35575e6d88
Genesis Lontok <deving.genesis@gmail.com>
2026-08-18 14:17:29 +0800

    Fixed and added folder and file layout; Modified App.jsx

---
+++ src/assets/pages/ElectricityBill.jsx
@@ -0,0 +1,9 @@
+function ElectricityBill() {
+  return (
+    <>
+      <h1>Hello, Raymond!</h1>
+    </>
+  );
+}
+
+export default ElectricityBill;
---
+++ src/assets/pages/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <>
+      <h1>Hello, Genesis</h1>
+    </>
+  );
+}
+
+export default Login;
